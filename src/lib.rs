04fda606176918fb6806e7424fe5c36b1a479bb3
Lucio Franco <luciofranco14@gmail.com>
2020-01-18 19:40:11 -0500

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,8 @@
+mod queue;
+mod stack;
 mod sync;
+
+pub use stack::Stack;
 
 #[cfg(test)]
 mod tests;
-
-use std::ptr;
-use sync::{
-    atomic::{AtomicPtr, Ordering},
-    CausalCell,
-};
-
-pub struct Stack<T> {
-    head: AtomicPtr<Node<T>>,
-}
-
-struct Node<T> {
-    value: CausalCell<Option<T>>,
-
-    next: *mut Node<T>,
-}
-
-impl<T: std::fmt::Debug> Stack<T> {
-    pub fn new() -> Self {
-        Self {
-            head: AtomicPtr::new(ptr::null_mut()),
-        }
-    }
-
-    pub fn push(&self, value: T) {
-        let mut new_head = Box::new(Node {
-            value: CausalCell::new(Some(value)),
-
-            next: ptr::null_mut(),
-        });
-
-        let old_head = self.head.load(Ordering::Relaxed);
-
-        new_head.next = old_head;
-
-        let new_head = Box::into_raw(new_head);
-        while self
-            .head
-            .compare_and_swap(old_head, new_head, Ordering::AcqRel)
-            != old_head
-        {}
-    }
-
-    pub fn pop(&self) -> Option<T> {
-        let old_head = self.head.load(Ordering::Relaxed);
-
-        if old_head == ptr::null_mut() {
-            return None;
-        }
-
-        let new_head = unsafe { &mut *old_head }.next;
-
-        while self
-            .head
-            .compare_and_swap(old_head, new_head, Ordering::AcqRel)
-            != old_head
-        {}
-
-        let old = unsafe { Box::from_raw(old_head) };
-        let value = unsafe { old.value.with_mut(|p| (*p).take()) };
-
-        value
-    }
-}
--- src/tests.rs
+++ src/tests.rs
@@ -8,16 +8,34 @@ fn loom_stack() {
     loom::model(|| {
         let stack = Arc::new(Stack::new());
 
-        stack.push(0);
+        const NUM: usize = 10;
 
         let stack1 = stack.clone();
-        thread::spawn(move || {
-            stack1.push(1);
-        })
-        .join()
-        .unwrap();
+        let j1 = thread::spawn(move || {
+            for i in 0..NUM {
+                stack1.push((1, i));
+            }
+        });
 
-        assert_eq!(stack.pop(), Some(1));
-        assert_eq!(stack.pop(), Some(0));
+        let stack2 = stack.clone();
+        let j2 = thread::spawn(move || {
+            for i in 0..NUM {
+                stack2.push((2, i));
+            }
+        });
+
+        j1.join().unwrap();
+        j2.join().unwrap();
+
+        let mut res = Vec::new();
+
+        while let Some(value) = stack.pop() {
+            res.push(value);
+        }
+
+        for i in 0..NUM {
+            assert!(res.iter().any(|v| v == &(1, i)));
+            assert!(res.iter().any(|v| v == &(2, i)));
+        }
     });
 }
